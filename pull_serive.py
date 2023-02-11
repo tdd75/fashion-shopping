10a579fbc89ef435c863eb0dc40fa7ea030b049a
tdd75 <tranducduy7520@gmail.com>
2023-02-11 21:38:55 +0700

    feat: init project

---
+++ pull_serive.py
@@ -0,0 +1,16 @@
+import os
+
+services = {
+    'fashion_shopping_admin': 'fashion-shopping-admin',
+    'fashion_shopping_backend': 'fashion-shopping-backend',
+    'fashion_shopping_chatbot': 'fashion-shopping-chatbot',
+    'fashion_shopping_image_search': 'fashion-shopping-image-search',
+}
+
+github_url = 'https://github.com/tdd75/'
+
+for folder, repo in services.items():
+    if not os.path.exists(folder):
+        os.system(f'git clone {github_url + repo}')
+    else:
+        os.system(f'cd {folder} && git pull && cd ..')
