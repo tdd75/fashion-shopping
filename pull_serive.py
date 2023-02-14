52d4a13f51e66f9d744eaa9595cb7928b8e0c1f1
tdd75 <tranducduy7520@gmail.com>
2023-02-14 07:02:23 +0000

    feat: new config nginx

--- pull_serive.py
+++ pull_serive.py
@@ -2,6 +2,7 @@ import os
 
 services = {
     'fashion_shopping_admin': 'fashion-shopping-admin',
+    'fashion_shopping_app': 'fashion-shopping-app',
     'fashion_shopping_backend': 'fashion-shopping-backend',
     'fashion_shopping_chatbot': 'fashion-shopping-chatbot',
     'fashion_shopping_image_search': 'fashion-shopping-image-search',
@@ -11,6 +12,6 @@ github_url = 'https://github.com/tdd75/'
 
 for folder, repo in services.items():
     if not os.path.exists(folder):
-        os.system(f'git clone {github_url + repo}')
+        os.system(f'git clone {github_url + repo} {folder}')
     else:
         os.system(f'cd {folder} && git pull && cd ..')
